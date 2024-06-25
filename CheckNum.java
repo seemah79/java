c687a6588ab8648e820bc67e09c27dc0db3f408c
seemah79 <seemah.xworkz@gmail.com>
2024-06-25 22:51:26 +0530

    the work is

---
+++ CheckNum.java
@@ -0,0 +1,35 @@
+public class CheckNum{
+public static void main(String args[]){
+    int[] numbers ={20,50,23,4,35,7,91,27,97};
+	
+	int even = 0;
+	int odd = 0;
+  for(int index = 0; index<numbers.length; index++){ 
+  if (numbers[index] % 2==0){
+	 System.out.println("the even numbers are:" +numbers[index] );
+	 
+	 }else
+     
+	 
+	 
+	  
+  
+     
+      System.out.println("the numbers are:" +numbers[index]); 
+      
+  
+  
+
+
+
+
+	 
+
+
+
+
+
+  }
+
+}
+}

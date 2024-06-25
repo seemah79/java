7e305cc71553d429a9857660f32d8555d9c5c518
seemah79 <seemah.xworkz@gmail.com>
2024-06-25 22:42:06 +0530

    array work is

---
+++ Sum.java
@@ -0,0 +1,26 @@
+public class Sum{
+public static void main(String args[]){
+    int[] numbers ={2,5,7,17,77};
+	int Sum=0;
+  for(int index = 0; index<numbers.length; index++){ 
+    Sum=Sum+numbers[index]; 
+  }
+     System.out.println("the sum of the numbers are:" +Sum );
+        
+      
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}

63ea6ce50ca06d9f41b52f8a693ef57ce8b0e71b
seemah79 <seemah.xworkz@gmail.com>
2024-06-25 22:46:02 +0530

    Array

---
+++ Order.java
@@ -0,0 +1,16 @@
+public class Order{
+
+public static void main(String args[]){
+ int Num[] = {8,7,6,5,4,3,2,1};
+  for(int i=Num.length-1; i>=0; i-- ){
+  
+  System.out.println("Reverse order is:" + Num[i]);
+  
+  
+
+
+
+
+}
+}
+}

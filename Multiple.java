a3cb4203da11b8c453d4f284c3263949ef82c6f6
seemah79 <seemah.xworkz@gmail.com>
2024-06-25 22:47:01 +0530

    Array

---
+++ Multiple.java
@@ -0,0 +1,15 @@
+  public class Multiple{
+  public static void main(String args[]){
+  int Num[] = {3,6,9,12,15,18,21,24,27,30};
+  
+  for( int i=0; i<Num.length; i++ ){
+	  
+	  
+  
+  if (Num[i]% 3 == 0);
+  System.out.println("Multiples of 3 numbers is:" +Num[i]);
+  
+  }
+  
+  }
+  }

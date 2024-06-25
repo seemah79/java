9fb7274459b417f6ded76ed86363ca370938ed7e
seemah79 <seemah.xworkz@gmail.com>
2024-06-25 22:57:46 +0530

    access is

---
+++ CheckinRunner.java
@@ -0,0 +1,14 @@
+public class CheckinRunner{
+
+public static void main(String args[]){
+ int sum=0;
+  for(int index = 0; index< Checkin.numbers.length; index++){ 
+    sum=sum+Checkin.numbers[index]; 
+  
+
+System.out.println("the sum of numbers are:" + Checkin.numbers[index].sum[index]);
+
+
+}
+}
+}

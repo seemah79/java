469f2343bb7ef5fe3b7b8a7add9ab41a697eb2a3
seemah79 <seemah.xworkz@gmail.com>
2024-06-25 09:56:52 +0530

    Array Operations

---
+++ Numbers.java
@@ -0,0 +1,21 @@
+public class Numbers{
+
+ public static void main(String args []){
+     int[] Numbers = {1,2,3,4,5,6};
+    for (int i = 0; i<Numbers.length; i++){
+	
+	System.out.println("Elements of the numbers:" +Numbers[i]);
+	}	
+		
+		System.out.println("Length of the numbers:" + Numbers.length);
+
+	
+
+
+	
+
+
+
+	
+}
+}
